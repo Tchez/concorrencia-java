9dcce0d6c5007503f630b02c82e819a92dafca39
Marco Antônio <marco.netto@mjv.com.br>
2024-03-09 13:26:49 -0300

    feat: :sparkles: melhorando implementação da Classe Room

--- src/Room.java
+++ src/Room.java
@@ -1,17 +1,19 @@
 package src;
 
 import java.util.LinkedList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Room {
-    LinkedList<Seat> seats = new LinkedList<>();
-    private int rows;
-    private int seatsPerRow;
+    private final LinkedList<Seat> seats = new LinkedList<>();
+    private final int rows;
+    private final int seatsPerRow;
+    private final Lock seatLock = new ReentrantLock();
 
     public Room(int rows, int seatsPerRow) {
         this.rows = rows;
         this.seatsPerRow = seatsPerRow;
-
-        this.createSeats();
+        createSeats();
     }
 
     private void createSeats() {
@@ -22,37 +24,37 @@ public class Room {
         }
     }
 
-    public void printSeats() {
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < seatsPerRow; j++) {
-                System.out.print(seats.get(i * seatsPerRow + j).getPosition() + " ");
-            }
-            System.out.println();
+    public void reserveSeat(String position) {
+        Seat seat = getSeatByPosition(position);
+
+        if (seat != null && seat.isEmpty()) {
+            seat.setEmpty(false);
         }
     }
 
-    public void occupySeat(String position) {
-        for (Seat seat : seats) {
-            if (seat.getPosition().equals(position)) {
-                seat.setEmpty(false);
-            }
-        }
-    }
+    public void cancelSeatReservation(String position) {
+        Seat seat = getSeatByPosition(position);
 
-    public void freeSeat(String position) {
-        for (Seat seat : seats) {
-            if (seat.getPosition().equals(position)) {
-                seat.setEmpty(true);
-            }
+        if (seat != null && !seat.isEmpty()) {
+            seat.setEmpty(true);
         }
     }
 
     public boolean isSeatEmpty(String position) {
-        for (Seat seat : seats) {
-            if (seat.getPosition().equals(position)) {
-                return seat.isEmpty();
-            }
+        Seat seat = getSeatByPosition(position);
+
+        return seat != null && seat.isEmpty();
+    }
+
+    public Seat getSeatByPosition(String position) {
+        seatLock.lock();
+        try {
+            return seats.stream()
+                    .filter(seat -> seat.getPosition().equals(position))
+                    .findFirst()
+                    .orElse(null);
+        } finally {
+            seatLock.unlock();
         }
-        return false;
     }
 }
